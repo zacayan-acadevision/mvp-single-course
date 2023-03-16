2da5c72f1159f7d69d3b31b2c4e64ab850faeee6
Federico Zacayan <federico.zacayan@gmail.com>
2023-03-16 06:13:37 -0300

    minor changes

--- src/components/Course.js
+++ src/components/Course.js
@@ -5,20 +5,19 @@ function Course() {
     return (
         <div className="d-flex flex-raw " >
             <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{ width: '280px', height: '100vh' }}>
-                <h1 className="fs-4 text">This is the landing course</h1>
+                <h1 className="fs-4 text">Curso de React-Query</h1>
                 <iframe width="100%" src="https://www.youtube.com/embed/X1qo8qEaWLg" title="YouTube video player"
-                    frameborder="0"
+                    frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen></iframe>
+                    allowFullScreen></iframe>
                     <div style={{marginBottom: 'auto'}}>
                         <ul>
-                            <li>Leccin 1</li>
-                            <li>Leccin 1</li>
-                            <li>Leccin 1</li>
-                            <li>Leccin 1</li>
-                            <li>Leccin 1</li>
-                            <li>Leccin 1</li>
-                            <li>Leccin 1</li>
+                            <li><a href="#">Leccion 1</a></li>
+                            <li><a href="#">Leccion 2</a></li>
+                            <li><a href="#">Leccion 3</a></li>
+                            <li><a href="#">Leccion 4</a></li>
+                            <li><a href="#">Leccion 5</a></li>
+                            <li><a href="#">Leccion 6</a></li>
                         </ul>
                     </div>
                 
